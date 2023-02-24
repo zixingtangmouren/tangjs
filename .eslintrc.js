d27d3787196ce531b8404a94240de88ca4d5f752
tangmouren <1678977488@qq.com>
2023-02-25 01:33:10 +0800

    refactor: 重构 create-tang

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/prefer-ts-expect-error': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
   },
 };
---
+++ packages/create-tang/src/config.ts
@@ -0,0 +1,8 @@
+import { defineTemplate } from '@tangjs/octopus';
+
+export default [
+  defineTemplate({
+    name: 'simple-demo',
+    description: 'simple-demo',
+  }),
+];
